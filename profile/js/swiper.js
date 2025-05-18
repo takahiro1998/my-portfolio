f9c69401edce84240524c510543bcb6885ad1052
takahiro1998 <tokkan001@gmail.com>
2025-05-18 17:06:01 +0900

    slide：スライドエリアの微調整

--- profile/js/swiper.js
+++ profile/js/swiper.js
@@ -1,7 +1,18 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
-  slidesPerView: 4,
-  spaceBetween: 20,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  breakpoints: {
+    1000: {
+      slidesPerView: 3,
+    },
+    600: {
+      slidesPerView: 2, // タブレット以上では3枚
+    },
+    0: {
+      slidesPerView: 1, // スマホでは1枚
+    }
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
