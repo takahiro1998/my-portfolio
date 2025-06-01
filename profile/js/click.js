50fade55d3321e1f58c92823fb299d4329fad870
takahiro1998 <tokkan001@gmail.com>
2025-06-01 21:32:38 +0900

    送信条件を変更

--- profile/js/click.js
+++ profile/js/click.js
@@ -7,3 +7,32 @@ menuLinks.forEach(link => {
     menuToggle.checked = false;
   });
 });
+
+// 関連サイトのリンク遷移防止POP表示・非表示
+    document.querySelectorAll('.not-link').forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.getElementById('production').classList.remove('hidden');
+      });
+    });
+
+    document.getElementById('production-close').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.getElementById('production').classList.add('hidden');
+    })
+
+//  コメント送信後のページ遷移防止+POP表示
+document.getElementById('commentForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const username = this.username.value;
+  const comment = this.format.value;
+
+  if (!username || !comment) {
+    alert('お名前とコメント両方を入力の上、送信ください。');
+    return;
+  }
+
+  alert('コメントありがとう！');
+  this.reset();
+});
