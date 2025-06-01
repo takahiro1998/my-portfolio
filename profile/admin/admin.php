2e3fbeccdbad7fcab08776bc934b979eb1b27d33
takahiro1998 <tokkan001@gmail.com>
2025-06-01 19:05:08 +0900

    gitテスト

--- profile/admin/admin.php
+++ profile/admin/admin.php
@@ -2,6 +2,7 @@
 <?php
 $name = isset($_POST["username"]) ? htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8') : '';
 $comment = isset($_POST["format"]) ? htmlspecialchars($_POST["format"], ENT_QUOTES, 'UTF-8') : '';
+$time = date("Y-m-d H:i:s");
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +14,12 @@ $comment = isset($_POST["format"]) ? htmlspecialchars($_POST["format"], ENT_QUOT
   <link rel="stylesheet" href="../scss/admin.css">
 </head>
 <body>
-  <header><div class="flex"><h1>Portfolio</h1></div></header>
-  <h1>コメント一覧A</h1>
+  <header><div class="flex"><h1>管理ページ</h1></div></header>
+  <h2>コメント一覧</h2>
   <ul>
     <li><?php echo $name; ?></li>
     <li><?php echo $comment; ?></li>
   </ul>
+
 </body>
 </html>
